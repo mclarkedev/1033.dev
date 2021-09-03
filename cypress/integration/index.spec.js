960ecb58285f95e5efea260efbfd400104217846
mclarke <mclarkestudio@gmail.com>
2021-09-03 15:32:02 -0400

    feat(home): home title

---
+++ cypress/integration/index.spec.js
@@ -0,0 +1,8 @@
+describe('Home (index.js)', () => {
+    it('should render "Matthew"', () => {
+
+      // Visit local index page and check for 'Matthew'
+      cy.visit('http://localhost:3000/')
+      cy.get('h1').contains('Matthew')
+    })
+  })
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <title>
+                    Matthew develops software
+                </title>
+            </Head>
+            <main>
+                <h1>
+                    Matthew develops ☼ software
+                </h1>
+            </main>
+        </>
+    )
+}
