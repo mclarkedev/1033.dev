0c7616d8d5aabac27d4b3d3547c6e08ee2bc05f4
mclarke <mclarkestudio@gmail.com>
2021-09-03 15:46:52 -0400

    chore(prettier): format to code style

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,8 +1,7 @@
 describe('Home (index.js)', () => {
-    it('should render "Matthew"', () => {
-
-      // Visit local index page and check for 'Matthew'
-      cy.visit('http://localhost:3000/')
-      cy.get('h1').contains('Matthew')
-    })
-  })
+  it('should render "Matthew"', () => {
+    // Visit local index page and check for 'Matthew'
+    cy.visit('http://localhost:3000/');
+    cy.get('h1').contains('Matthew');
+  });
+});
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
-import Head from 'next/head'
-import React from 'react'
+import Head from 'next/head';
+import React from 'react';
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-                <title>
-                    Matthew develops software
-                </title>
-            </Head>
-            <main>
-                <h1>
-                    Matthew develops ☼ software
-                </h1>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Matthew develops software</title>
+      </Head>
+      <main>
+        <h1>Matthew develops ☼ software</h1>
+      </main>
+    </>
+  );
 }
