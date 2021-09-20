4c93ff82a63f3b1338572dd557c0b026da1cdb82
mclarke <mclarkestudio@gmail.com>
2021-09-19 20:01:44 -0400

    feat(home): add marquee

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import React from 'react';
-import Grid from '../components/Grid';
+import Marquee from 'react-fast-marquee';
 
 import { attributes } from '../content/index.md';
-import styles from '../styles/index.module.css';
+import Grid from '../components/Grid';
 
 export default function Home() {
   const { title, posts } = attributes;
@@ -14,10 +14,19 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title>Matthew develops software</title>
       </Head>
-      <header className="bg-white p-4 pl-8 pt-5 w-screen fixed z-50">
-        <h1 className={styles.title}>{title}</h1>
+      <header className="bg-white p-4 pl-8 pt-5 w-screen fixed z-50 flex row-span-2">
+        <h1 className="font-semibold text-lg flex-shrink-0">{title}</h1>
+        <div className="mt-2 ml-12 font-mono text-xs">
+          <Marquee>
+            Designer turned developer with 7+ years of experience designing art
+            and technology products and 3 years developing software
+            applications. Designer turned developer with 7+ years of experience
+            designing art and technology products and 3 years developing
+            software applications.
+          </Marquee>
+        </div>
       </header>
-      <main className={styles.mainContainer}>
+      <main className="p-6 overflow-hidden">
         <Grid data={posts} />
       </main>
     </>
